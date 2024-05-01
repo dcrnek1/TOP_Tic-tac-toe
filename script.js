a74a509c33701d6e9223a717e762c7d73b91fd89
dcrnek1 <dario.crnek@gmail.com>
2024-05-01 19:14:57 +0200

    Finalized

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const DOM = (() => {  
+    const writeSymbol = (symbol, x, y) => {
+        const cell = document.querySelector(`#r${x}_c${y}`);
+        cell.textContent = symbol;
+    }
+
+    const cells = document.querySelectorAll(".cell");
+    const clearBoard = () => {
+        cells.forEach((cell) => {
+            cell.textContent = '';
+        })
+    }
+
+    const lockBoard = () => {
+        cells.forEach((cell) => {
+            cell.classList.remove("cell-enabled");
+        })
+    }
+
+    const unlockBoard = () => {
+        cells.forEach((cell) => {
+            cell.classList.add("cell-enabled");
+        })
+    }
+
+    const lockCell = (rowIndex, colIndex) => {
+        document.querySelector(`#r${rowIndex}_c${colIndex}`).classList.remove("cell-enabled");
+    }
+
+    const message = document.querySelector(".result__message");
+    const writeMessage = (text) => {
+        message.textContent = text;
+    }
+
+    const clearMessage = () => {
+        message.textContent = '';
+    }
+
+    const btnNextRound = document.querySelector(".result__next-round");
+    const toggleButtonVisability = () => {
+        btnNextRound.classList.toggle("hidden");
+    }
+
+    const playerOneScoreEl = document.querySelector(".score-player1");
+    const playerTwoScoreEl = document.querySelector(".score-player2");
+    const writeScore = (playerOneScore, playerTwoScore) => {
+        playerOneScoreEl.textContent = playerOneScore;
+        playerTwoScoreEl.textContent = playerTwoScore;
+    }
+
+    return {writeSymbol, clearBoard, writeMessage, clearMessage, toggleButtonVisability, writeScore, lockBoard, lockCell, unlockBoard};
+})();
+
 const gameBoard = (() => {
     let board = [
         ["", "", ""],
@@ -17,8 +70,10 @@ const gameBoard = (() => {
             if (board) return board;
         }  
     };
+
     const clearBoard = () => {
        board.forEach(row => row.fill(""));
+       DOM.clearBoard();
     }
 
     return {getBoard, write, clearBoard, isLocked, toggleLock};
@@ -40,7 +95,7 @@ const game = (() => {
         "one": player("Dario", "X"),
         "two": player("Hrvoje", "O"),
     }
-
+    
     //will foreach through them and check if last player have any combination equal.
     const winCombs = [
         [[0, 0], [0, 1], [0, 2]], 
@@ -54,13 +109,18 @@ const game = (() => {
     ]
 
     let lastPlayer = players.two;
+    DOM.writeMessage(`${players.one.getName()}'s turn.`)
 
     const play = (rowIndex, colIndex) => {
         //Fetch current player
         let currentPlayer = players.one.getSymbol() !== lastPlayer.getSymbol() ? players.one : players.two;
+        
         //Write players symbol inside gameboard
-        //If he tried to write to the j
         if (gameBoard.write(currentPlayer.getSymbol(), rowIndex, colIndex)) {
+            DOM.writeSymbol(currentPlayer.getSymbol(), rowIndex, colIndex);
+            DOM.writeMessage(`${lastPlayer.getName()}'s turn.`)
+            DOM.lockCell(rowIndex, colIndex);
+
             //Check if current user has won
             checkIfUserWon(currentPlayer);
             lastPlayer = currentPlayer;
@@ -69,20 +129,22 @@ const game = (() => {
 
     const checkIfUserWon = (player) => {
         const board = gameBoard.getBoard();
-        console.table(board);
-        console.log("Player: " + player.getName());
-        
         const playerSymbol = player.getSymbol();
 
         for (let i = 0; i < winCombs.length; i++) {
 
             if (board[winCombs[i][0][0]][winCombs[i][0][1]] === playerSymbol && board[winCombs[i][1][0]][winCombs[i][1][1]] === playerSymbol && board[winCombs[i][2][0]][winCombs[i][2][1]] === playerSymbol) {
-                console.log(player.getName() + " has won");
+                DOM.writeMessage(player.getName() +" has won!!!");
+                DOM.toggleButtonVisability();
+
                 player.increaseScore();
+                DOM.writeScore(players.one.getScore(), players.two.getScore());
                 gameBoard.toggleLock();
+                DOM.lockBoard();
                 break;
             } else if (i == winCombs.length - 1 && !board.flat().includes("")) {
-                console.log("Its a draw");
+                DOM.writeMessage("It's a draw :(");
+                DOM.toggleButtonVisability();
                 gameBoard.toggleLock();
             }
         }
@@ -93,11 +155,22 @@ const game = (() => {
         if (gameBoard.isLocked()) {
             gameBoard.toggleLock();
         }
+        DOM.unlockBoard();
+
+        const current_playerName = players.one.getSymbol() !== lastPlayer.getSymbol() ? players.one.getName() : players.two.getName();
+        DOM.writeMessage(`${current_playerName}'s turn.`);
+        DOM.toggleButtonVisability();
     }
 
     return {play, startNextRound};
 })();
 
-const userInteractions = (() => {
+document.querySelectorAll(".cell").forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+        game.play(e.target.id[1], e.target.id[4]);
+    })
+})
 
-})();
+document.querySelector(".result__next-round").addEventListener("click", (e) => {
+    game.startNextRound();
+})
